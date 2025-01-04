b936f8777b7669ab0728beac6bf1fa0700935bd2
Z <angelasvv@126.com>
2025-01-04 18:42:57 +0800

    new Emulated

--- firmware/src/our_descriptor.cc
+++ firmware/src/our_descriptor.cc
@@ -309,6 +309,67 @@ const uint8_t our_report_descriptor_horipad[] = {
     0xC0,              // End Collection
 };
 
+const uint8_t our_report_descriptor_spacemouse_pro[] = {
+    0x05, 0x01,       // Usage Page (Generic Desktop Ctrls)
+    0x09, 0x08,       // Usage (Multi-axis Controller)
+    0xA1, 0x01,       // Collection (Application)
+    0x05, 0x09,       //   Usage Page (Button)
+    0x15, 0x00,       //   Logical Minimum (0)
+    0x25, 0x01,       //   Logical Maximum (1)
+    0x19, 0x01,       //   Usage Minimum (0x01)
+    0x29, 0x09,       //   Usage Maximum (0x09)
+    0x95, 0x09,       //   Report Count (9)
+    0x75, 0x01,       //   Report Size (1)
+    0x81, 0x02,       //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x95, 0x01,       //   Report Count (1)
+    0x75, 0x02,       //   Report Size (2)
+    0x81, 0x03,       //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x09, 0x0C,       //   Usage (0x0C)
+    0x95, 0x01,       //   Report Count (1)
+    0x75, 0x01,       //   Report Size (1)
+    0x81, 0x02,       //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x95, 0x01,       //   Report Count (1)
+    0x75, 0x04,       //   Report Size (4)
+    0x81, 0x03,       //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x05, 0x01,       //   Usage Page (Generic Desktop Ctrls)
+    0x09, 0x01,       //   Usage (Pointer)
+    0xA1, 0x00,       //   Collection (Physical)
+    0x09, 0x30,       //     Usage (X)
+    0x09, 0x31,       //     Usage (Y)
+    0x09, 0x32,       //     Usage (Z)
+    0x09, 0x33,       //     Usage (Rx)
+    0x09, 0x34,       //     Usage (Ry)
+    0x09, 0x35,       //     Usage (Rz)
+    0x16, 0x0C, 0xFE, //     Logical Minimum (-500)
+    0x26, 0xF4, 0x01, //     Logical Maximum (500)
+    0x75, 0x10,       //     Report Size (16)
+    0x95, 0x06,       //     Report Count (6)
+    0x81, 0x02,       //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x05, 0x01,       //     Usage Page (Generic Desktop Ctrls)
+    0x09, 0x01,       //     Usage (Pointer)
+    0x09, 0x46,       //     Usage (Vno)
+    0x15, 0x00,       //     Logical Minimum (0)
+    0x26, 0xFF, 0x00, //     Logical Maximum (255)
+    0x75, 0x08,       //     Report Size (8)
+    0x95, 0x0E,       //     Report Count (14)
+    0xB1, 0x02,       //     Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x05, 0x08,       //     Usage Page (LEDs)
+    0x09, 0x3C,       //     Usage (Usage Multi Mode Indicator)
+    0xA1, 0x02,       //     Collection (Logical)
+    0x09, 0x43,       //       Usage (Slow Blink On Time)
+    0x15, 0x00,       //       Logical Minimum (0)
+    0x25, 0x0F,       //       Logical Maximum (15)
+    0x75, 0x04,       //       Report Size (4)
+    0x95, 0x01,       //       Report Count (1)
+    0x91, 0x02,       //       Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0,             //     End Collection
+    0x75, 0x04,       //     Report Size (4)
+    0x95, 0x01,       //     Report Count (1)
+    0x91, 0x03,       //     Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0,             //   End Collection
+    0xC0,             // End Collection
+}; // 116 bytes
+
 uint8_t const our_report_descriptor_ps4[] = {
     0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
     0x09, 0x05,        // Usage (Game Pad)
@@ -667,6 +728,14 @@ const our_descriptor_def_t our_descriptors[] = {
         .handle_received_report = do_handle_received_report,
         .clear_report = xac_compat_clear_report,
         .default_value = ps4_stadia_default_value,  // sic
+    },
+    {
+        .idx = 6,
+        .descriptor = our_report_descriptor_spacemouse_pro,
+        .descriptor_length = sizeof(our_report_descriptor_spacemouse_pro),
+        .vid = 0x046D, // 0x046D
+        .pid = 0xC62b, // 0xC62b
+        .handle_received_report = do_handle_received_report,
     },
 };
 
